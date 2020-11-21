1cfb77f3e17e86a67bc861b94d5bfbad81c0ec35
James Lynch <lynchjames@gmail.com>
2020-11-21 02:57:42 +0000

    Uses normalizePath from Obsidian API re #22

--- src/file.ts
+++ src/file.ts
@@ -1,5 +1,5 @@
 import { now } from 'moment';
-import type { Vault } from 'obsidian';
+import { Vault, normalizePath } from 'obsidian';
 import { DAY_PLANNER_DEFAULT_CONTENT, DAY_PLANNER_FILENAME } from './constants';
 import MomentDateRegex from './moment-date-regex';
 import { DayPlannerSettings, DayPlannerMode, NoteForDateQuery } from './settings';
@@ -47,7 +47,7 @@ export default class DayPlannerFile {
 
     async createFolderIfNotExists(path: string){
         try {
-            const normalizedPath = this.normalizePath(path);
+            const normalizedPath = normalizePath(path);
             const folderExists = await this.vault.adapter.exists(normalizedPath, false)
             if(!folderExists) {
               await this.vault.createFolder(normalizedPath);
@@ -59,7 +59,7 @@ export default class DayPlannerFile {
 
     async createFileIfNotExists(fileName: string) {
         try {
-            const normalizedFileName = this.normalizePath(fileName);
+            const normalizedFileName = normalizePath(fileName);
             if (!await this.vault.adapter.exists(normalizedFileName, false)) {
                 await this.vault.create(normalizedFileName, DAY_PLANNER_DEFAULT_CONTENT);
             }
@@ -80,35 +80,9 @@ export default class DayPlannerFile {
     async updateFile(fileName: string, fileContents: string){
         this.prepareFile();
         try {
-            return await this.vault.adapter.write(this.normalizePath(fileName), fileContents);
+            return await this.vault.adapter.write(normalizePath(fileName), fileContents);
         } catch (error) {
             console.log(error)
         }
-    }
-
-    private normalizePath(path: string) : string {
-        // Always use forward slash
-        path = path.replace(/\\/g, '/');
-  
-        // Strip start/end slash
-        while (path.startsWith('/') && path !== '/') {
-            path = path.substr(1);
-        }
-        while (path.endsWith('/') && path !== '/') {
-            path = path.substr(0, path.length - 1);
-        }
-        
-        // Use / for root
-        if (path === '') {
-            path = '/';
-        }
-    
-        path = path
-            // Replace non-breaking spaces with regular spaces
-            .replace('\u00A0', ' ')
-            // Normalize unicode to NFC form
-            .normalize('NFC');
-        
-        return path;
     }
 }
