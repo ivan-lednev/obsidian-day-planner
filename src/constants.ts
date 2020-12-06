e496e624983e6ab651ba97b8ea47cc0c01fc6505
James Lynch <lynchjames@gmail.com>
2020-12-06 00:07:01 +0000

    Fixes incorrect matching of BREAK and END keywords re #40

--- src/constants.ts
+++ src/constants.ts
@@ -3,9 +3,9 @@ export const DATE_REGEX = /(?<target>{{date:?(?<date>[^}]*)}})/g;
 
 export const DAY_PLANNER_FILENAME = 'Day Planner-{{date}}.md';
 
-//https://regex101.com/r/VAxRnc/3
+//https://regex101.com/r/VAxRnc/4
 export const PLAN_PARSER_REGEX = 
-/^(((-?[\s]*\[?(?<completion>[x ]*)\])?(\d.)?\s*?(?<hours>\d{1,2}):(?<minutes>\d{2})\s)((?<break>BREAK)|(?<end>END|finish)|((?<text>.*))))|((?<unmatched>[ \t]*- .*))$/gmi;
+/^(((-?[\s]*\[?(?<completion>[x ]*)\])?(\d.)?\s*?(?<hours>\d{1,2}):(?<minutes>\d{2})\s)((?<break>BREAK)$|(?<end>END)$|((?<text>.*))))$|((?<unmatched>[ \t]*- .*))/gmi;
 
 export const MERMAID_REGEX = /```mermaid\ngantt[\S\s]*?```\s*/gmi;
 
