f36a416e4caf1949e64b0a145778555ce513a696
James Lynch <lynchjames@gmail.com>
2020-12-23 22:32:17 +0000

    Regex parse ignores lines starting with HH:mm re #42

--- src/constants.ts
+++ src/constants.ts
@@ -3,9 +3,9 @@ export const DATE_REGEX = /(?<target>{{date:?(?<date>[^}]*)}})/g;
 
 export const DAY_PLANNER_FILENAME = 'Day Planner-{{date}}.md';
 
-//https://regex101.com/r/VAxRnc/6
+//https://regex101.com/r/VAxRnc/8
 export const PLAN_PARSER_REGEX = 
-/^(((-?[\s]*\[?(?<completion>[x ]*)\])?(\d.)?\s*?(?<hours>\d{1,2}):(?<minutes>\d{2})\s)((?<break>BREAK[\n ]?)|(?<end>END[\n ]?)|((?<text>.*))))$/gmi;
+/^(((-?[\s]*\[?(?<completion>[x ]*)\])(\d.)?\s*?(?<hours>\d{1,2}):(?<minutes>\d{2})\s)((?<break>BREAK[\n ]?)|(?<end>END[\n ]?)|((?<text>.*))))$/gmi;
 
 export const MERMAID_REGEX = /```mermaid\ngantt[\S\s]*?```\s*/gmi;
 
