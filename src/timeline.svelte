17c3cebb0568e3322a7b82bb4a08d952002ec62b
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2021-02-02 16:35:12 +0100

    add data-title attribute for event item
    With a Data Attribute, users will be able to access specific data attributes in their CSS for more customisation

--- src/timeline.svelte
+++ src/timeline.svelte
@@ -361,7 +361,7 @@ color:#fff;
         
       <div class="events">
         {#each summary.items as item, i}
-            <div class="event_item event_item_color{i%10+1} {shortClass(item)} {pastClass(item)}" style="height: {item.durationMins*timelineZoomLevel}px;">
+            <div class="event_item event_item_color{i%10+1} {shortClass(item)} {pastClass(item)}" style="height: {item.durationMins*timelineZoomLevel}px;" data-title="{item.rawTime}">
               <div class="event_item_contents">
                 <div class="ei_Dot {item === summary.current ? 'dot_active' : ''}"></div>
                 <div class="ei_Title">{item.rawTime}</div>
