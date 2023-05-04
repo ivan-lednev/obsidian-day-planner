574f469be3258580319c72745264f83756fa0d53
Georgios KOTZAMPOPOULOS <georgios.kotzampopoulos@tutanota.com>
2023-05-04 16:34:08 +0200

    (#202) Fix gnatt task duration suffix generation
    Fixes #202

--- src/mermaid.ts
+++ src/mermaid.ts
@@ -48,7 +48,7 @@ export default class PlannerMermaid {
         const currentMoment = moment(item.time);
         const nextMoment = moment(next.time);
         const untilNext = Math.floor(moment.duration(nextMoment.diff(currentMoment)).asMinutes());
-        return ', ' + untilNext + 'mm';
+        return ', ' + untilNext + 'm';
     }
 
     private escape(input: string){
