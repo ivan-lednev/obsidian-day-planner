ea7832199201364df55d77762432d476d53d6302
Ivan Liadniou <ivan_liadniou@epam.com>
2023-07-30 16:21:46 +0200

    Clean up header UI, add date

--- src/ui/components/timeline.svelte
+++ src/ui/components/timeline.svelte
@@ -18,40 +18,45 @@
 </script>
 
 <div class="controls">
-  <SettingsButton isActive={settingsVisible} onClick={toggleSettings} />
-  <div>
-    <div class="setting-item">
+  <div class="header">
+    <span class="date">Jul 30, 2023, Sunday</span>
+    <SettingsButton isActive={settingsVisible} onClick={toggleSettings} />
+  </div>
+  {#if settingsVisible}
+    <div>
+      <div class="setting-item">
+        <div class="setting-item-info">
+          <div class="setting-item-name">Zoom</div>
+        </div>
+        <div class="setting-item-control">
+          <select class="dropdown">
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+            <option value="4">4</option>
+          </select>
+        </div>
+      </div>
+    </div>
+
+    <div class="setting-item mod-toggle" style:display="none">
       <div class="setting-item-info">
-        <div class="setting-item-name">Zoom</div>
+        <div class="setting-item-name">Collapse results</div>
+        <div class="setting-item-description"></div>
       </div>
       <div class="setting-item-control">
-        <select class="dropdown">
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-        </select>
+        <div
+          class="checkbox-container mod-small"
+          class:is-enabled={someSetting}
+          on:click={() => {
+            someSetting = !someSetting;
+          }}
+        >
+          <input type="checkbox" tabindex="0" />
+        </div>
       </div>
     </div>
-  </div>
-
-  <div class="setting-item mod-toggle" style:display="none">
-    <div class="setting-item-info">
-      <div class="setting-item-name">Collapse results</div>
-      <div class="setting-item-description"></div>
-    </div>
-    <div class="setting-item-control">
-      <div
-        class="checkbox-container mod-small"
-        class:is-enabled={someSetting}
-        on:click={() => {
-          someSetting = !someSetting;
-        }}
-      >
-        <input type="checkbox" tabindex="0" />
-      </div>
-    </div>
-  </div>
+  {/if}
 </div>
 <div class="scroller">
   <div class="time-grid">
@@ -69,6 +74,12 @@
 </div>
 
 <style>
+  .date {
+    font-size: 1.2em;
+    color: var(--text-muted);
+    flex: 1 0 0;
+  }
+
   .setting-item {
     padding: var(--size-2-3) 0;
     border: none;
@@ -80,10 +91,13 @@
 
   .controls {
     padding: var(--size-4-2);
-    padding-bottom: 0;
     display: flex;
     flex-direction: column;
     border-bottom: 1px solid var(--background-modifier-border);
+  }
+
+  .header {
+    display: flex;
   }
 
   .scroller {
