0f1dc837fe2034eb0ac3d74a43feedf0562716ee
Ivan Lednev <bishop1860@gmail.com>
2023-08-07 16:00:10 +0200

    Remove weekly view code form timeline

--- src/ui/components/timeline.svelte
+++ src/ui/components/timeline.svelte
@@ -4,7 +4,7 @@
   import Needle from "./needle.svelte";
   import TaskContainer from "./task-container.svelte";
   import Controls from "./controls.svelte";
-  import { startHour, tasks, weeklyTasks } from "../../store/timeline-store";
+  import { startHour, tasks } from "../../store/timeline-store";
 
   let userHoversOverScroller = false;
 
@@ -29,14 +29,10 @@
 >
   <div class="scale-with-days">
     <Ruler {visibleHours} />
-    {#each $weeklyTasks as tasksForDay}
-      <Column {visibleHours}>
-        {#if tasksForDay.isToday}
-          <Needle scrollBlockedByUser={userHoversOverScroller} />
-        {/if}
-        <TaskContainer tasks={tasksForDay.tasks} />
-      </Column>
-    {/each}
+    <Column {visibleHours}>
+      <Needle scrollBlockedByUser={userHoversOverScroller} />
+      <TaskContainer tasks={$tasks} />
+    </Column>
   </div>
 </div>
 
