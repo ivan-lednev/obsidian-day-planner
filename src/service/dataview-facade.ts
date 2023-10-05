4685c3b9a0a12a3676caacc1734348a417e900e1
ivan-lednev <bishop1860@gmail.com>
2023-10-05 14:36:50 +0200

    feat: add way to extract plan items from dataview

--- src/service/dataview-facade.ts
+++ src/service/dataview-facade.ts
@@ -1,9 +1,50 @@
-import { DataArray, DataviewApi, STask } from "obsidian-dataview";
+import { Moment } from "moment";
+import { DataviewApi, STask } from "obsidian-dataview";
+
+import { createPlanItem } from "../parser/parser";
+import { PlanItem } from "../types";
+import { getId } from "../util/id";
+import { getDiffInMinutes, getMinutesSinceMidnight } from "../util/moment";
+
+function sTaskToPlanItem(sTask: STask): PlanItem {
+  const { startTime, endTime } = createPlanItem({
+    line: `- ${sTask.text}`,
+    completeContent: `- ${sTask.text}`,
+    day: window.moment(sTask.scheduled.toMillis()),
+    location: {
+      path: sTask.path,
+      line: sTask.line,
+    },
+  });
+
+  return {
+    startTime,
+    rawStartTime: "-",
+    rawEndTime: "-",
+    listTokens: `${sTask.symbol} `,
+    firstLineText: sTask.text,
+    text: sTask.text,
+    durationMinutes: getDiffInMinutes(endTime, startTime),
+    startMinutes: getMinutesSinceMidnight(startTime),
+    location: {
+      path: sTask.path,
+      line: sTask.line,
+    },
+    id: getId(),
+  };
+}
 
 export class DataviewFacade {
-  constructor(private readonly dataview: DataviewApi) {}
+  constructor(private readonly dataview: () => DataviewApi) {}
 
-  main() {
-    return this.dataview.pages()["file"]["tasks"] as DataArray<STask>;
+  getTasksFor(day: Moment): PlanItem[] {
+    return this.dataview()
+      .pages()
+      .file.tasks.where(
+        (task: STask) =>
+          task.scheduled &&
+          window.moment(task.scheduled.toMillis()).isSame(day, "day"),
+      )
+      .map(sTaskToPlanItem);
   }
 }
