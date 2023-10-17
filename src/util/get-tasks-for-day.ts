7f8fefb8b46a3429b9e0cb0855075a9fdc720341
ivan-lednev <bishop1860@gmail.com>
2023-10-17 18:02:21 +0200

    fix: do not show task from daily note if it's scheduled for another day
    resolves: #285

--- src/util/get-tasks-for-day.ts
+++ src/util/get-tasks-for-day.ts
@@ -1,4 +1,5 @@
 import { Moment } from "moment/moment";
+import { TFile } from "obsidian";
 import { getAllDailyNotes, getDailyNote } from "obsidian-daily-notes-interface";
 import { DataArray, STask } from "obsidian-dataview";
 
@@ -6,35 +7,43 @@ import { timeFromStartRegExp } from "../regexp";
 import { sTaskToPlanItem } from "../service/dataview-facade";
 import { PlanItem } from "../types";
 
+function isScheduledForThisDay(task: STask, day: Moment) {
+  if (!task.scheduled) {
+    return false;
+  }
+
+  const scheduledMoment = window.moment(task.scheduled.toMillis());
+
+  return scheduledMoment.isSame(day, "day");
+}
+
+function isTimeSetOnTask(task: STask) {
+  return timeFromStartRegExp.test(task.text);
+}
+
+function isTaskInFile(task: STask, file?: TFile) {
+  return task.path === file?.path;
+}
+
+function isScheduledForAnotherDay(task: STask, day: Moment) {
+  return task.scheduled && !isScheduledForThisDay(task, day);
+}
+
 export function getTasksForDay(day: Moment, dataviewTasks: DataArray<STask>) {
   if (dataviewTasks.length === 0) {
     return [];
   }
 
-  const noteForDay = getDailyNote(day, getAllDailyNotes());
+  const noteForThisDay = getDailyNote(day, getAllDailyNotes());
 
   return dataviewTasks
-    .where((task: STask) => {
-      const timeIsSet = timeFromStartRegExp.test(task.text);
-
-      if (!timeIsSet) {
-        return false;
-      }
-
-      const isInDailyNote = task.path === noteForDay?.path;
-
-      if (isInDailyNote) {
-        return true;
-      }
-
-      if (!task.scheduled) {
-        return false;
-      }
-
-      const scheduledMoment = window.moment(task.scheduled.toMillis());
-
-      return scheduledMoment.isSame(day, "day");
-    })
+    .where(
+      (task: STask) =>
+        isTimeSetOnTask(task) &&
+        !isScheduledForAnotherDay(task, day) &&
+        (isScheduledForThisDay(task, day) ||
+          isTaskInFile(task, noteForThisDay)),
+    )
     .map((sTask: STask) => sTaskToPlanItem(sTask, day))
     .sort((task: PlanItem) => task.startMinutes)
     .array();
