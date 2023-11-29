eeb17014837b58817cd83a315ee392138ac6a2ad
Philip Weinke <philip.weinke@digitalnoise.de>
2023-11-29 07:45:10 +0100

    refactor: scheduled tasks loader for readability

--- src/planned-items/loader/scheduled-tasks-loader.ts
+++ src/planned-items/loader/scheduled-tasks-loader.ts
@@ -3,14 +3,6 @@ import { DataviewApi } from "obsidian-dataview";
 import Day from "../day";
 import { PlannedItem, PlannedItemLoader } from "../planned-items";
 
-function isScheduledForThisDay(task: PlannedItem, day: Day) {
-  if (!task?.scheduled?.toMillis) {
-    return false;
-  }
-
-  return day.equals(Day.fromMoment(window.moment(task.scheduled.toMillis())));
-}
-
 export default class ScheduledTasksLoader
   implements PlannedItemLoader<PlannedItem>
 {
@@ -18,27 +10,41 @@ export default class ScheduledTasksLoader
 
   public async forDays(days: Set<Day>): Promise<Map<Day, Array<PlannedItem>>> {
     const result = new Map();
+    const tasks = await this.tasksScheduledFor(days);
 
+    for (const day of days) {
+      result.set(
+        day,
+        tasks.filter((task) => this.isScheduledFor(task, day)),
+      );
+    }
+
+    return result;
+  }
+
+  private async tasksScheduledFor(days: Set<Day>): Promise<Array<PlannedItem>> {
+    const queryResult = await this.dataview.query(this.taskQueryFor(days));
+    if ("error" in queryResult) {
+      console.error(`Error querying data: ${queryResult.error}`);
+      return [];
+    }
+
+    return queryResult.value.values as Array<PlannedItem>;
+  }
+
+  private taskQueryFor(days: Set<Day>): string {
     const conditions = Array.from(days.values())
       .map((d) => `scheduled = date(${d.asIso()})`)
       .join(" OR ");
 
-    const queryResult = await this.dataview.query(`TASK WHERE ${conditions}`);
-    if ("error" in queryResult) {
-      console.error(`Error querying data: ${queryResult.error}`);
-      return result;
+    return `TASK WHERE ${conditions}`;
+  }
+
+  private isScheduledFor(task: PlannedItem, day: Day): boolean {
+    if (!task?.scheduled?.toMillis) {
+      return false;
     }
 
-    const dataviewTasks = queryResult.value.values as Array<PlannedItem>;
-
-    for (const day of days) {
-      const tasks = dataviewTasks.filter((task) =>
-        isScheduledForThisDay(task, day),
-      );
-
-      result.set(day, tasks);
-    }
-
-    return result;
+    return day.equals(Day.fromMoment(window.moment(task.scheduled.toMillis())));
   }
 }
