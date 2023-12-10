bb2f636ad1fd62154f656dcdc2be9e1d59f0d8f1
ivan-lednev <bishop1860@gmail.com>
2023-12-10 13:11:34 +0100

    feat: render markdown in active clocks

--- src/ui/components/time-tracker/active-clocks.svelte
+++ src/ui/components/time-tracker/active-clocks.svelte
@@ -6,36 +6,32 @@
   import { ObsidianContext } from "../../../types";
   import ControlButton from "../control-button.svelte";
   import Tree from "../obsidian/tree.svelte";
+  import Task from "../task.svelte";
+  import { sTaskToUnscheduledTask } from "../../../service/dataview-facade";
 
   const { activeClocks, clockOut, cancelClock } =
     getContext<ObsidianContext>(obsidianContext);
 </script>
 
 <Tree title="Active clocks">
-<!--  TODO: -> sTasksWithActiveClocks -->
+  <!--  TODO: -> sTasksWithActiveClocks -->
   {#each $activeClocks as sTask}
-    <div class="inner-container">
-      <ControlButton label="Clock out" on:click={() => clockOut(sTask)}>
-        <Pause class="svg-icon" />
-      </ControlButton>
-      <ControlButton label="Cancel clock" on:click={() => cancelClock(sTask)}>
-        <Trash2 class="svg-icon" />
-      </ControlButton>
-      <div class="task-text">{sTask.text}</div>
-    </div>
+    <Task task={sTaskToUnscheduledTask(sTask, window.moment())}>
+      <div class="buttons">
+        <ControlButton label="Clock out" on:click={() => clockOut(sTask)}>
+          <Pause class="svg-icon" />
+        </ControlButton>
+        <ControlButton label="Cancel clock" on:click={() => cancelClock(sTask)}>
+          <Trash2 class="svg-icon" />
+        </ControlButton>
+      </div>
+    </Task>
   {/each}
 </Tree>
 
 <style>
-  /*   TODO: rename classes */
-  .inner-container {
+  .buttons {
     display: flex;
-  }
-
-  .task-text {
-    display: flex;
-    align-items: center;
-    margin-left: var(--size-4-2);
-    font-size: var(--font-ui-small);
+    flex-direction: column;
   }
 </style>
