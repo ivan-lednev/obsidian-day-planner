4cf36b3f5d476fab0ca33578fe0c5a834b8d64c4
ivan-lednev <bishop1860@gmail.com>
2024-11-12 14:36:31 +0100

    feat: add dummy categories widget

---
+++ src/ui/components/tracker/categories.svelte
@@ -0,0 +1,51 @@
+<div class="categories">
+  <div
+    style:--category-color-rgb="var(--color-green-rgb)"
+    style:width="20%"
+    class="category"
+  >
+    #work
+  </div>
+  <div
+    style:--category-color-rgb="var(--color-blue-rgb)"
+    style:width="30%"
+    class="category"
+  >
+    #home
+  </div>
+  <div
+    style:--category-color-rgb="var(--color-yellow-rgb)"
+    style:width="50%"
+    class="category"
+  >
+    #gym
+  </div>
+</div>
+
+<style>
+  .categories {
+    overflow: hidden;
+    display: flex;
+
+    height: var(--size-4-8);
+    margin: var(--size-4-2) var(--size-4-3);
+
+    border: 1px solid var(--background-modifier-border);
+    border-radius: var(--radius-s);
+  }
+
+  .category {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: var(--font-ui-smaller);
+    font-weight: var(--font-medium);
+
+    background-color: rgba(var(--category-color-rgb), 0.4);
+  }
+
+  .category:not(:last-child) {
+    border-right: 1px solid var(--background-modifier-border);
+  }
+</style>
