c26a03944f08d7824548d5fb2b8a60be473fb1b9
LoricAndre <loric.andre@pm.me>
2025-03-03 18:21:18 +0100

    feat: add remote event details modal

--- src/additional-svelte-typings.d.ts
+++ src/additional-svelte-typings.d.ts
@@ -2,5 +2,7 @@ declare namespace svelteHTML {
   interface HTMLAttributes {
     "on:tap"?: () => void;
     "on:longpress"?: () => void;
+    "task-summary"?: string;
+    "task-description"?: string;
   }
 }
