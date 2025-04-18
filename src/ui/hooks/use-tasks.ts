08c0ecfc014b6c423b451745ba9379eccdee0957
Ivan Lednev <bishop1860@gmail.com>
2025-04-18 12:28:22 +0200

    refactor: clean up use-tasks hook

---
+++ src/ui/hooks/use-tasks.ts
@@ -0,0 +1,151 @@
+import { flow, groupBy, uniqBy } from "lodash/fp";
+import type { Moment } from "moment";
+import type { MetadataCache } from "obsidian";
+import { derived, type Readable, type Writable } from "svelte/store";
+
+import { addHorizontalPlacing } from "../../overlap/overlap";
+import { selectRemoteTasks } from "../../redux/ical/ical-slice";
+import type { createUseSelector } from "../../redux/use-selector";
+import { DataviewFacade } from "../../service/dataview-facade";
+import { WorkspaceFacade } from "../../service/workspace-facade";
+import type { DayPlannerSettings } from "../../settings";
+import type { LocalTask, Task, WithTime } from "../../task-types";
+import type { OnUpdateFn, PointerDateTime } from "../../types";
+import { hasClockProp } from "../../util/clock";
+import * as dv from "../../util/dataview";
+import { withClockMoments } from "../../util/dataview";
+import * as m from "../../util/moment";
+import { getDayKey, getRenderKey } from "../../util/task-utils";
+
+import { useDataviewTasks } from "./use-dataview-tasks";
+import { useEditContext } from "./use-edit/use-edit-context";
+import { useNewlyStartedTasks } from "./use-newly-started-tasks";
+import { useTasksWithActiveClockProps } from "./use-tasks-with-active-clock-props";
+import { useVisibleDailyNotes } from "./use-visible-daily-notes";
+import { useVisibleDataviewTasks } from "./use-visible-dataview-tasks";
+
+export function useTasks(props: {
+  settingsStore: Writable<DayPlannerSettings>;
+  debouncedTaskUpdateTrigger: Readable<object>;
+  isOnline: Readable<boolean>;
+  visibleDays: Readable<Moment[]>;
+  layoutReady: Readable<boolean>;
+  dataviewFacade: DataviewFacade;
+  metadataCache: MetadataCache;
+  currentTime: Readable<Moment>;
+  workspaceFacade: WorkspaceFacade;
+  onUpdate: OnUpdateFn;
+  pointerDateTime: Readable<PointerDateTime>;
+  useSelector: ReturnType<typeof createUseSelector>;
+}) {
+  const {
+    settingsStore,
+    visibleDays,
+    layoutReady,
+    debouncedTaskUpdateTrigger,
+    dataviewFacade,
+    metadataCache,
+    currentTime,
+    workspaceFacade,
+    pointerDateTime,
+    onUpdate,
+    useSelector,
+  } = props;
+
+  const remoteTasks = useSelector(selectRemoteTasks);
+
+  const visibleDailyNotes = useVisibleDailyNotes(
+    layoutReady,
+    debouncedTaskUpdateTrigger,
+    visibleDays,
+  );
+
+  const dataviewTasks = useDataviewTasks({
+    dataviewFacade,
+    metadataCache,
+    settings: settingsStore,
+    visibleDailyNotes,
+    refreshSignal: debouncedTaskUpdateTrigger,
+  });
+
+  const tasksWithActiveClockProps = useTasksWithActiveClockProps({
+    dataviewTasks,
+  });
+
+  const tasksWithActiveClockPropsAndDurations = derived(
+    [tasksWithActiveClockProps, currentTime],
+    ([$tasksWithActiveClockProps, $currentTime]) =>
+      $tasksWithActiveClockProps.map((task: LocalTask) => ({
+        ...task,
+        durationMinutes: m.getDiffInMinutes(
+          $currentTime,
+          task.startTime.clone().startOf("minute"),
+        ),
+        truncated: ["bottom" as const],
+      })),
+  );
+
+  const visibleTasksWithClockProps = derived(
+    [dataviewTasks, tasksWithActiveClockPropsAndDurations],
+    ([$dataviewTasks, $tasksWithActiveClockPropsAndDurations]) => {
+      const flatTasksWithClocks = $dataviewTasks
+        .filter(hasClockProp)
+        .flatMap(withClockMoments)
+        .map(dv.toTaskWithClock)
+        .concat($tasksWithActiveClockPropsAndDurations);
+
+      return groupBy(
+        ({ startTime }) => getDayKey(startTime),
+        flatTasksWithClocks,
+      );
+    },
+  );
+
+  function getDisplayedTasksWithClocksForTimeline(day: Moment) {
+    return derived(
+      visibleTasksWithClockProps,
+      ($visibleTasksWithClockProps) => {
+        const tasksForDay = $visibleTasksWithClockProps[getDayKey(day)] || [];
+
+        return flow(uniqBy(getRenderKey), addHorizontalPlacing)(tasksForDay);
+      },
+    );
+  }
+
+  const localTasks = useVisibleDataviewTasks(dataviewTasks, visibleDays);
+
+  const tasksWithTimeForToday = derived(
+    [localTasks, remoteTasks, currentTime],
+    ([$localTasks, $remoteTasks, $currentTime]: [Task[], Task[], Moment]) => {
+      return $localTasks
+        .concat($remoteTasks)
+        .filter(
+          (task): task is WithTime<Task> =>
+            task.startTime.isSame($currentTime, "day") && !task.isAllDayEvent,
+        );
+    },
+  );
+
+  const editContext = useEditContext({
+    workspaceFacade,
+    onUpdate,
+    settings: settingsStore,
+    localTasks,
+    remoteTasks,
+    pointerDateTime,
+  });
+
+  const newlyStartedTasks = useNewlyStartedTasks({
+    settings: settingsStore,
+    tasksWithTimeForToday,
+    currentTime,
+  });
+
+  return {
+    tasksWithActiveClockProps,
+    getDisplayedTasksWithClocksForTimeline,
+    tasksWithTimeForToday,
+    editContext,
+    newlyStartedTasks,
+  };
+}
--- src/util/create-hooks.svelte.ts
+++ src/util/create-hooks.svelte.ts
@@ -1,6 +1,5 @@
-import { flow, groupBy, uniqBy } from "lodash/fp";
 import type { Moment } from "moment";
-import { App, MetadataCache } from "obsidian";
+import { App } from "obsidian";
 import {
   derived,
   fromStore,
@@ -12,7 +11,6 @@ import {
 
 import { reQueryAfterMillis } from "../constants";
 import type DayPlanner from "../main";
-import { addHorizontalPlacing } from "../overlap/overlap";
 import { dataviewChange as dataviewChangeAction } from "../redux/dataview/dataview-slice";
 import {
   darkModeUpdated,
@@ -20,35 +18,23 @@ import {
   keyDown as keyDownAction,
   networkStatusChanged,
 } from "../redux/global-slice";
-import { selectRemoteTasks } from "../redux/ical/ical-slice";
 import type { AppDispatch } from "../redux/store";
 import { createUseSelector } from "../redux/use-selector";
 import { DataviewFacade } from "../service/dataview-facade";
 import { WorkspaceFacade } from "../service/workspace-facade";
 import type { DayPlannerSettings } from "../settings";
-import type { LocalTask, Task, WithTime } from "../task-types";
 import type { OnUpdateFn, PointerDateTime } from "../types";
 import { useDataviewChange } from "../ui/hooks/use-dataview-change";
 import { useDataviewLoaded } from "../ui/hooks/use-dataview-loaded";
-import { useDataviewTasks } from "../ui/hooks/use-dataview-tasks";
 import { useDateRanges } from "../ui/hooks/use-date-ranges";
 import { useDebounceWithDelay } from "../ui/hooks/use-debounce-with-delay";
-import { useEditContext } from "../ui/hooks/use-edit/use-edit-context";
 import { useIsOnline } from "../ui/hooks/use-is-online";
 import { useKeyDown } from "../ui/hooks/use-key-down";
 import { useModPressed } from "../ui/hooks/use-mod-pressed";
-import { useNewlyStartedTasks } from "../ui/hooks/use-newly-started-tasks";
-import { useTasksWithActiveClockProps } from "../ui/hooks/use-tasks-with-active-clock-props";
-import { useVisibleDailyNotes } from "../ui/hooks/use-visible-daily-notes";
-import { useVisibleDataviewTasks } from "../ui/hooks/use-visible-dataview-tasks";
+import { useTasks } from "../ui/hooks/use-tasks";
 import { useVisibleDays } from "../ui/hooks/use-visible-days";
-import * as m from "../util/moment";
 
-import { hasClockProp } from "./clock";
-import * as dv from "./dataview";
-import { withClockMoments } from "./dataview";
 import { getUpdateTrigger } from "./store";
-import { getDayKey, getRenderKey } from "./task-utils";
 
 interface CreateHooksProps {
   app: App;
@@ -64,136 +50,6 @@ interface CreateHooksProps {
 
 function getDarkModeFlag() {
   return document.body.hasClass("theme-dark");
-}
-
-export function useTasks(props: {
-  settingsStore: Writable<DayPlannerSettings>;
-  debouncedTaskUpdateTrigger: Readable<object>;
-  isOnline: Readable<boolean>;
-  visibleDays: Readable<Moment[]>;
-  layoutReady: Readable<boolean>;
-  dataviewFacade: DataviewFacade;
-  metadataCache: MetadataCache;
-  currentTime: Readable<Moment>;
-  workspaceFacade: WorkspaceFacade;
-  onUpdate: OnUpdateFn;
-  pointerDateTime: Readable<PointerDateTime>;
-  useSelector: ReturnType<typeof createUseSelector>;
-}) {
-  const {
-    settingsStore,
-    visibleDays,
-    layoutReady,
-    debouncedTaskUpdateTrigger,
-    dataviewFacade,
-    metadataCache,
-    currentTime,
-    workspaceFacade,
-    pointerDateTime,
-    onUpdate,
-    useSelector,
-  } = props;
-
-  const remoteTasks = useSelector(selectRemoteTasks);
-
-  const visibleDailyNotes = useVisibleDailyNotes(
-    layoutReady,
-    debouncedTaskUpdateTrigger,
-    visibleDays,
-  );
-
-  const dataviewTasks = useDataviewTasks({
-    dataviewFacade,
-    metadataCache,
-    settings: settingsStore,
-    visibleDailyNotes,
-    refreshSignal: debouncedTaskUpdateTrigger,
-  });
-
-  const tasksWithActiveClockProps = useTasksWithActiveClockProps({
-    dataviewTasks,
-  });
-
-  const tasksWithActiveClockPropsAndDurations = derived(
-    [tasksWithActiveClockProps, currentTime],
-    ([$tasksWithActiveClockProps, $currentTime]) =>
-      $tasksWithActiveClockProps.map((task: LocalTask) => ({
-        ...task,
-        // We keep time resolution to the minute, so that all the elements move
-        //  in sync on the UI when time changes
-        durationMinutes: m.getDiffInMinutes(
-          $currentTime,
-          task.startTime.clone().startOf("minute"),
-        ),
-        truncated: ["bottom" as const],
-      })),
-  );
-
-  const visibleTasksWithClockProps = derived(
-    [dataviewTasks, tasksWithActiveClockPropsAndDurations],
-    ([$dataviewTasks, $tasksWithActiveClockPropsAndDurations]) => {
-      const flatTasksWithClocks = $dataviewTasks
-        .filter(hasClockProp)
-        .flatMap(withClockMoments)
-        .map(dv.toTaskWithClock)
-        .concat($tasksWithActiveClockPropsAndDurations);
-
-      return groupBy(
-        ({ startTime }) => getDayKey(startTime),
-        flatTasksWithClocks,
-      );
-    },
-  );
-
-  // todo: remove duplication
-  function getDisplayedTasksWithClocksForTimeline(day: Moment) {
-    return derived(
-      visibleTasksWithClockProps,
-      ($visibleTasksWithClockProps) => {
-        const tasksForDay = $visibleTasksWithClockProps[getDayKey(day)] || [];
-
-        return flow(uniqBy(getRenderKey), addHorizontalPlacing)(tasksForDay);
-      },
-    );
-  }
-
-  const localTasks = useVisibleDataviewTasks(dataviewTasks, visibleDays);
-
-  const tasksWithTimeForToday = derived(
-    [localTasks, remoteTasks, currentTime],
-    ([$localTasks, $remoteTasks, $currentTime]: [Task[], Task[], Moment]) => {
-      return $localTasks
-        .concat($remoteTasks)
-        .filter(
-          (task): task is WithTime<Task> =>
-            task.startTime.isSame($currentTime, "day") && !task.isAllDayEvent,
-        );
-    },
-  );
-
-  const editContext = useEditContext({
-    workspaceFacade,
-    onUpdate,
-    settings: settingsStore,
-    localTasks,
-    remoteTasks,
-    // todo: doesn't have to know about pointers
-    pointerDateTime,
-  });
-
-  const newlyStartedTasks = useNewlyStartedTasks({
-    settings: settingsStore,
-    tasksWithTimeForToday,
-    currentTime,
-  });
-
-  return {
-    tasksWithActiveClockProps,
-    getDisplayedTasksWithClocksForTimeline,
-    tasksWithTimeForToday,
-    editContext,
-    newlyStartedTasks,
-  };
 }
 
 export function createHooks({
