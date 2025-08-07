0b5cf23ac99638a1034c6bc09b66f1710ed795b7
Ivan Lednev <bishop1860@gmail.com>
2025-08-07 13:40:11 +0200

    refactor: fix type errors

--- src/util/with-notice.ts
+++ src/util/with-notice.ts
@@ -7,8 +7,12 @@ export function withNotice<T, U>(fn: (...args: T[]) => U | Promise<U>) {
       await fn(...args);
     } catch (error: unknown) {
       console.error(error);
-      // @ts-ignore
-      new Notice(error?.message || error);
+
+      if (error instanceof Error) {
+        new Notice(error.message);
+      } else {
+        new Notice(String(error));
+      }
     }
   };
 }
