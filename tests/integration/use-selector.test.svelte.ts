8fb5cb4fe190632f37d92b3683f7f530994ee5c8
Ivan Lednev <bishop1860@gmail.com>
2026-03-20 15:47:02 +0100

    test: remove broken test

--- tests/integration/use-selector.test.svelte.ts
+++ tests/integration/use-selector.test.svelte.ts
@@ -33,41 +33,10 @@ describe("useSelector", () => {
     expect(logEntries.current).toHaveLength(0);
   });
 
-  test("Does not re-run on unrelated store changes", async () => {
-    const { useSelector, dispatch } = await setUp();
-
-    let calledCount = 0;
-    let activeDays = $state(["2025-01-18"]);
-
-    const logEntries = useSelector((state) =>
-      selectLogEntriesForDayKeys(state, activeDays),
-    );
-
-    const derived = $derived.by(() => {
-      calledCount++;
-
-      return logEntries.current;
-    });
-
-    expect(derived).toHaveLength(1);
-    expect(calledCount).toBe(1);
-
-    dispatch(editCanceled());
-    activeDays = ["2024-01-01"];
-
-    expect(derived).toHaveLength(0);
-    expect(calledCount).toBe(2);
-
-    dispatch(editCanceled());
-
-    expect(derived).toHaveLength(0);
-    expect(calledCount).toBe(3);
-  });
-
   // todo: need to update some Svelte & Vitest deps to make this work
   //  This is an example straight from Svelte docs
   //  Tried it on a clean svelte kit app, it still didn't work
-  test.fails("Effects", () => {
+  test.fails("Effects can be tested", () => {
     function logger(getValue: () => any) {
       const log: any[] = $state([]);
 
@@ -101,4 +70,6 @@ describe("useSelector", () => {
 
     cleanup();
   });
+
+  test.todo("Does not re-run on unrelated store changes");
 });
