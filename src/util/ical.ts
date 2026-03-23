f63c51ac569e3a7542af31dda57ea90c28b64ac5
Ivan Lednev <bishop1860@gmail.com>
2026-03-23 18:08:29 +0100

    fix: prevent exdates from being different in different time zones

--- src/util/ical.ts
+++ src/util/ical.ts
@@ -38,18 +38,12 @@ function hasExceptionForDate(icalEvent: ical.VEvent, date: Date) {
     return false;
   }
 
-  // NOTE: exdate contains floating dates, i.e. any UTC offset that's on them
-  // must be ignored, and we should treat them as local time
-  const asMoment = window.moment(date);
-  const utcOffset = asMoment.utcOffset();
-  const dateWithoutOffset = asMoment.clone().subtract(utcOffset, "minutes");
-
   return Object.values(icalEvent.exdate).some((exceptionDate) => {
     if (!(exceptionDate instanceof Date)) {
       throw new Error("Unexpected exdate format");
     }
 
-    return window.moment(exceptionDate).isSame(dateWithoutOffset, "day");
+    return getIcalDayKey(exceptionDate) === getIcalDayKey(date);
   });
 }
 
