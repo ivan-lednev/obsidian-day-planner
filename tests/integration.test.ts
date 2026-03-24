c9a23816c6ccfcba466a8a702c7a21280974db4a
Ivan Lednev <bishop1860@gmail.com>
2026-03-24 14:20:07 +0100

    test: remove tests for old logic

--- tests/integration.test.ts
+++ tests/integration.test.ts
@@ -188,75 +188,8 @@ describe("Log Records with indexes", () => {
   test.todo("Old versions don't overwrite new ones");
 
   test.todo("Renaming a file removes entries by the old path");
-});
-
-describe("Clocks", () => {
-  test("Reads log data", async () => {
-    const { getDisplayedTasksWithClocksForTimeline } = await setUp({
-      visibleDays: ["2025-07-19"],
-    });
-
-    const displayedTasks = getDisplayedTasksWithClocksForTimeline(
-      window.moment("2025-07-19"),
-    );
-
-    expect(get(displayedTasks)).toEqual(
-      expect.arrayContaining([
-        expect.objectContaining({
-          startTime: window.moment("2025-07-19 12:00"),
-          durationMinutes: 150,
-        }),
-        expect.objectContaining({
-          startTime: window.moment("2025-07-19 15:00"),
-          durationMinutes: 90,
-        }),
-        expect.objectContaining({
-          startTime: window.moment("2025-07-19 13:00"),
-          durationMinutes: 210,
-        }),
-      ]),
-    );
-  });
-
-  test.todo("Only reads props under tasks");
 
   test.todo("Ignores invalid dates, extra keys");
-
-  test("Tasks with active clocks receive durations set to current time", async () => {
-    const {
-      getDisplayedTasksWithClocksForTimeline,
-      currentTime,
-      tasksWithActiveClockProps,
-    } = await setUp({
-      visibleDays: ["2025-01-11"],
-    });
-
-    currentTime.set(window.moment("2025-01-01 17:30"));
-
-    const displayedClocks = getDisplayedTasksWithClocksForTimeline(
-      window.moment("2025-01-01"),
-    );
-
-    expect(get(displayedClocks)).toMatchObject([
-      {
-        startTime: window.moment("2025-01-01 17:00"),
-        durationMinutes: 30,
-      },
-      {
-        startTime: window.moment("2025-01-01 13:00"),
-        durationMinutes: 120,
-      },
-    ]);
-
-    expect(get(tasksWithActiveClockProps)).toMatchObject([
-      {
-        startTime: window.moment("2025-01-01 17:00"),
-        durationMinutes: 30,
-      },
-    ]);
-  });
-
-  test.todo("Splits log entries over midnight");
 
   test.todo("Parses code blocks under nested tasks");
 
